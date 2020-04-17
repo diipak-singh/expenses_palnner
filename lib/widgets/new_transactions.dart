f676ad40aa4097fb570445075036438e2251bcdd
Deepak Singh Baise <deepaksinghbaise@gmail.com>
2020-04-18 01:51:46 +0530

    added new transaction to the list

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,36 +1,42 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    //onChanged: (val) => amountInput = val,
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print(titleController.text);
-                    },
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                  ),
-                ],
-              ),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
             ),
-          );
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              //onChanged: (val) => amountInput = val,
+              controller: amountController,
+            ),
+            FlatButton(
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -44,7 +44,7 @@ class _UserTransactionsState extends State<UserTransactions> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(),
+        NewTransaction(_addNewTransaction),
         TransactionList(_userTransactions),
       ],
     );
